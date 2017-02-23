bf91822d8a0cd3ea4afcc355537dc09bb3ddcab3
liy <withliyh@gmail.com>
2017-02-23 11:15:16 +0800

    print more info

--- client/client.go
+++ client/client.go
@@ -1,23 +1,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 
 	kcp "github.com/xtaci/kcp-go"
 )
 
+var host = flag.String("host", "127.0.0.1:2000", "remote host address and port")
+
 func main() {
-	fmt.Println("vim-go")
-	client, err := kcp.DialWithOptions("liy.today:2000", nil, 10, 3)
+	flag.Parse()
+	client, err := kcp.DialWithOptions(*host, nil, 10, 3)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	n, err := client.Write([]byte("hello"))
+	fmt.Printf("connect to %s.\n", *host)
+
+	content := "hello"
+	n, err := client.Write([]byte(content))
 	if err != nil {
 		fmt.Printf("%s", err.Error())
+		return
 	}
-	fmt.Printf("%d\n", n)
+	fmt.Printf("send [%s] to target [%d] bytes\n", content, n)
+	var buf = make([]byte, 1024)
+	n, err = client.Read(buf)
+	if err != nil {
+		fmt.Printf("%s", err.Error())
+		return
+	}
+	fmt.Printf("rect [%s]\n", string(buf[0:n]))
 	<-time.After(10 * time.Second)
 }
